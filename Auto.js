8cfddbd8e187ccaecb7a4e16290c0f08bcb14cfc
carlos8ahdz <carlosarmando_hernandez@ucol.mx>
2019-02-05 19:37:01 -0600

    Se inició

---
+++ Auto.js
@@ -0,0 +1,9 @@
+class Auto {
+    constructor(marca, modelo, color, kilometraje) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.modelo = color;
+        this.kilometraje = 0;
+        this.estado = "Apagado";
+    }
+}
