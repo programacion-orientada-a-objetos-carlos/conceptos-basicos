0084fd52bf7055999a7ba45a300906657edf4841
carlos8ahdz <carlosarmando_hernandez@ucol.mx>
2019-02-07 19:16:35 -0600

    Done this

--- Auto.js
+++ Auto.js
@@ -1,9 +1,36 @@
-class Auto {
-    constructor(marca, modelo, color, kilometraje) {
+export default class Auto {
+    constructor(marca, modelo, color = "Blanco", kilometraje = 0) {
         this.marca = marca;
         this.modelo = modelo;
-        this.modelo = color;
-        this.kilometraje = 0;
+        this.color = color;
+        this.kilometraje = kilometraje;
         this.estado = "Apagado";
+    }
+
+        mostrarEstado(){
+        console.log(
+            `El auto ${this.marca} ${this.color} del año ${
+            this.modelo
+        } tiene un kilometraje de ${this.kilometraje} kms y está ${this.estado}`
+        );
+        }
+
+    encender(){
+        this.estado = "Encendido";
+        this.mostrarEstado();
+    }
+    apagar(){
+        this.estado = "Apagado";
+        this.mostrarEstado();
+    }
+    avanzar(velocidad, tiempo){
+        if(this.estado === "Apagado"){
+            console.log("El auto no puede avanzar por que está apagado");
+        }
+        else {
+            let distancia = velocidad * tiempo;
+            this.kilometraje = this.kilometraje + distancia;
+            this.mostrarEstado();
+        }
     }
 }
--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-console.log('Hola, mundo!');
+import Auto from "./Auto.js";
+
+let auto1 = new Auto("Ford", 2000, "rojo", 100);
+let auto2 = new Auto("Nissan", 2010, "Azul",);
+let auto3 = new Auto("Toyota", 2018, "blanco");
+
+auto1.encender();
+auto1.avanzar(50, 1.5);
+auto1.apagar();
+auto1.avanzar(50, 1.5);
+
+console.log(`Kilometraje ${auto1.kilometraje}`);
+auto1.kilometraje = 5;
+console.log(`Kilometraje ${auto1.kilometraje}`);
