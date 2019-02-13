86bd1cf7120625c0c270f0bdfceff8b7712e07fa
carlos8ahdz <carlosarmando_hernandez@ucol.mx>
2019-02-12 18:45:34 -0600

    Done for today

--- main.js
+++ main.js
@@ -4,6 +4,10 @@ let auto1 = new Auto("Ford", 2000, "rojo", 100);
 let auto2 = new Auto("Nissan", 2010, "Azul",);
 let auto3 = new Auto("Toyota", 2018, "blanco");
 
+console.log(auto1.modelo);
+auto1.modelo = 2019;
+auto1.color = "Blanco";
+
 auto1.encender();
 auto1.avanzar(50, 1.5);
 auto1.apagar();
